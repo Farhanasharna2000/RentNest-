bf2c8108062b0e4f35c9082ffb26a269caf79234
Farhana Sharna <farhanasharna22@gmail.com>
2026-08-03 12:35:27 +0600

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../prisma/generated/prisma/client";
-
+console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
